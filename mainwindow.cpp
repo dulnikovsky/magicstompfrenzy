4251243606f1096168023ba6675e8765dfc24247
robert <robertvetter@alice.de>
2018-11-17 19:16:44 +0100

    Prepended licence header to each file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,26 @@
+/****************************************************************************
+**
+** Copyright (C) 2018 Robert Vetter.
+**
+** This file is part of the MagicstompFrenzy - an editor for Yamaha Magicstomp
+** effect processor
+**
+** THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+** ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+** IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+** PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+**
+** GNU General Public License Usage
+** This file may be used under the terms of the GNU
+** General Public License version 2.0 or (at your option) the GNU General
+** Public license version 3 or any later version . The licenses are
+** as published by the Free Software Foundation and appearing in the file LICENSE
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-2.0.html and
+** https://www.gnu.org/licenses/gpl-3.0.html.
+**/
+
 #include "mainwindow.h"
 #include "midiportmodel.h"
 #include "patchlistmodel.h"
